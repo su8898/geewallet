b92f1fccbe860faed622e8e8887339b324448c50
Afshin Arani <afshin@arani.dev>
2021-12-15 12:15:50 +0330

    Backend(LN): change to_self_delay to max possible amount
    Changed to_self_delay to maximum possible amount
    without breaking lnd compatiblity. This gives the gwallet
    user (or his watchtower) more time to get online and
    detect if the counterparty has broadcasted a revoked
    commitment tx.
    
    Numbers taken from:
    https://github.com/lightningnetwork/lnd/blob/6d59a2c807abd4dc2613bde14240221a941fb2a7/funding/manager.go#L64
    
    For more info about to_self_delay:
    https://github.com/lightning/bolts/blob/master/02-peer-protocol.md#the-open_channel-message

--- src/GWallet.Backend/UtxoCoin/Lightning/Settings.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/Settings.fs
@@ -50,7 +50,13 @@ module Settings =
             MaxHTLCValueInFlightMSat = LNMoney 10000L
             ChannelReserveSatoshis = funding * Config.ChannelReservePercentage / 100L
             HTLCMinimumMSat = LNMoney 1000L
-            ToSelfDelay = BlockHeightOffset16 6us
+            // see https://github.com/lightning/bolts/blob/master/02-peer-protocol.md#the-open_channel-message
+            ToSelfDelay =
+                match currency with
+                | BTC -> 2016us
+                | LTC -> 8064us
+                | _ -> failwith "Unsupported currency"
+                |> BlockHeightOffset16
             MaxAcceptedHTLCs = uint16 10
             Features = SupportedFeatures funding currency
         }
