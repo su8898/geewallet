ff09bcb8990c4a36776178826b70261bec090a09
Retheesh Erathu <sumod8898@gmail.com>
2022-01-25 17:53:53 +0200

    wip

--- src/GWallet.Backend/UtxoCoin/TorOperations.fs
+++ src/GWallet.Backend/UtxoCoin/TorOperations.fs
@@ -1,22 +1,11 @@
 ﻿namespace GWallet.Backend.UtxoCoin
 
 open System
-open System.Net.Sockets
-open System.Net
-open System.Diagnostics
-open System.IO
 
-open NBitcoin
-open DotNetLightning.Peer
-open DotNetLightning.Utils
-open ResultUtils.Portability
-open NOnion.Network
 open NOnion.Directory
 open NOnion.Services
 
 open GWallet.Backend
-open GWallet.Backend.FSharpUtil
-open GWallet.Backend.FSharpUtil.UwpHacks
 
 module internal TorOperations =
     let internal GetTorDirectory retryCount =
